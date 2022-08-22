4d8821b342ae9cd5a2478f428f52245db54729da
Tom Brown <herestomwiththeweather@gmail.com>
2022-08-21 20:43:18 -0500

    unprotected form to authorize a code

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -1,2 +1,7 @@
 class AuthorizationsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :authorizations, only: :create
+  get '/auth' => 'authorizations#new', as: :auth
 end
