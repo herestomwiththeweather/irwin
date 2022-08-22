7de8b551b201b18f8482b7ede30bbc1541010346
Tom Brown <herestomwiththeweather@gmail.com>
2022-08-21 23:24:31 -0500

    links to log in, log out, current_user method, rails g controller sessions new

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'Logged in!'
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Logged out!'
+  end
+
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :authorizations, only: :create
   get '/auth' => 'authorizations#new', as: :auth
+
+  get '/logout' => 'sessions#destroy', as: :logout
+  root 'sessions#home'
 end
