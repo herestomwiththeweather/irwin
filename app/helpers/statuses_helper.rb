46a33eab71d21666eea6551cce4bc01cd48532f1
Tom Brown <herestomwiththeweather@gmail.com>
2023-12-06 13:12:30 -0600

    text for status submit button now aware if it is a direct message

--- app/helpers/statuses_helper.rb
+++ app/helpers/statuses_helper.rb
@@ -1,5 +1,6 @@
 module StatusesHelper
-  def new_status_submit_text(status)
-    status.present? ? 'Reply' : 'Post'
+  def new_status_submit_text(status, direct_recipient_id)
+    prefix = direct_recipient_id.present? ? 'DM ' : ''
+    "#{prefix}#{status.present? ? 'Reply' : 'Post'}"
   end
 end
