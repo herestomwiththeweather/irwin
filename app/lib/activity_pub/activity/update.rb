115682ffba5f4e4be990101dd8c421d9ec0d54d4
Tom Brown <herestomwiththeweather@gmail.com>
2025-05-21 22:08:30 -0500

    reply update signature verification. related to gh-45

--- app/lib/activity_pub/activity/update.rb
+++ app/lib/activity_pub/activity/update.rb
@@ -1,7 +1,24 @@
 class ActivityPub::Activity::Update < ActivityPub::Activity
   def perform
     Rails.logger.info "#{self.class}##{__method__}"
-    UpdateJob.perform_later(@json, @account.id)
+    actor = @account
+
+    # since current account could be sending a received reply, we cannot assume current account created the status
+    if ('Note' == @json['object']['type']) && (@json['actor'] != @account.identifier)
+      Rails.logger.info "#{self.class}##{__method__} validating signature for: #{@json['actor']}"
+      account = Account.fetch_by_key(@json['signature']['creator'])
+      if account.nil?
+        Rails.logger.info "#{self.class}##{__method__} Error No account for #{@json['actor']}"
+        return 400
+      end
+
+      return 400 unless account.verify_signature(@json)
+      actor = account
+
+      Rails.logger.info "#{self.class}##{__method__} *** signature verification succeeded *** for actor: #{actor.id}"
+    end
+
+    UpdateJob.perform_later(@json, actor.id)
     202
   end
 end
