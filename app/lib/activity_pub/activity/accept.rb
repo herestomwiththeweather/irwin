ccd2565ed31a4f5abe77d948855fcef2e5c0cac9
Tom Brown <herestomwiththeweather@gmail.com>
2025-10-21 16:41:44 -0500

    safely reference account. closes gh-53

--- app/lib/activity_pub/activity/accept.rb
+++ app/lib/activity_pub/activity/accept.rb
@@ -3,7 +3,7 @@ class ActivityPub::Activity::Accept < ActivityPub::Activity
     Rails.logger.info "#{self.class}##{__method__}"
     Rails.logger.info "#{self.class}##{__method__} actor: #{@json['actor']}" # actor accepting the follow
     Rails.logger.info "#{self.class}##{__method__} follower: #{@json['object']['actor']}"
-    follower = User.by_actor(@json['object']['actor']).account
+    follower = User.by_actor(@json['object']['actor'])&.account
     follow = ::Follow.find_by(target_account: @account, account: follower)
     if follow.present?
       follow.accept!
